1739ff860b9b96bb6fedf2f9b688ebbfc2a5d2c5
Fouz <dhikifauzan97@gmail.com>
2025-05-27 07:39:44 +0700

    add cors

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
-// index.js
 import express from 'express';
+import cors from 'cors';
 import authRouter from './router/auth_router.js';
 import mentalHealthRouter from './router/mental_health_router.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/auth', authRouter);
